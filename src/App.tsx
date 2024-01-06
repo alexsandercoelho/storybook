2f1e2ab1421cc9b4440ba5b63b12bafe7bca9120
alexsandercoelho <a_pcoelho@hotmail.com>
2024-01-06 02:17:12 -0300

    first component create

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import "./styles/global.css";
 export function App() {
-    return (
-   <h1>teste</h1>
+  return (
+    <>
+      <h1 className="font-bold text-5xl text-violet-500">Nozes</h1>
 
-  )
+      <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white houver:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  );
 }
-
